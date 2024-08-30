67ed7e19a8c8c2d05323efbf516f45b5435eece1
Sergey Boykov <sutemi67@gmail.com>
2024-08-30 11:21:56 +0300

    patch

--- app/src/main/java/com/example/cleanarchitecturelecture/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecturelecture/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.cleanarchitecturelecture
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val getButton = findViewById<Button>(R.id.get_button)
+        val saveButton = findViewById<Button>(R.id.save_button)
+        val nameText = findViewById<TextView>(R.id.name_text)
+        val input = findViewById<EditText>(R.id.input)
     }
+
 }
