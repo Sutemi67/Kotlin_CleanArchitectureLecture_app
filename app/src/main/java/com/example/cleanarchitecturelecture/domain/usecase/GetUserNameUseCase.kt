0be23d17b825b19293f9667ff2ca80aebd1399d6
Sergey Boykov <sutemi67@gmail.com>
2024-08-30 11:18:29 +0300

    initial

---
+++ app/src/main/java/com/example/cleanarchitecturelecture/domain/usecase/GetUserNameUseCase.kt
@@ -0,0 +1,5 @@
+package com.example.cleanarchitecturelecture.domain.usecase
+
+class GetUserNameUseCase {
+    fun execute() {}
+}
---
+++ app/src/main/java/com/example/cleanarchitecturelecture/domain/usecase/SaveUserNameUseCase.kt
@@ -0,0 +1,5 @@
+package com.example.cleanarchitecturelecture.domain.usecase
+
+class SaveUserNameUseCase {
+    fun execute() {}
+}
